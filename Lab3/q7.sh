9eecc4d604dcfc767bfe1b8806a744e8d17774ed
Sameep1234 <sameep0112vani@gmail.com>
2021-09-15 15:18:00 +0530

    Commented Lab 7

--- Lab3/q7.sh
+++ Lab3/q7.sh
@@ -10,7 +10,16 @@ read -p "Mention the older date: (mm/dd/yy) " oldDate
 cd $DIR
 
 # List all files newer than a specified date
+echo "Files Newer than $newDate are"
 find . -type f -newermt "$newDate 0:00:00"
+# find = Search for files in directory hierarchy
+# . = Starting point of the search. By default it is '.' implying current active directory
+# -type = Represents type of file to be searched.
+# f = Stands for regular files
+# -newerbt = Filter for finding files that are "newer" (mt means modification time) than the specified date.
 
 # List all files older than a specified date
-find . -type f -not -newermt "$olderDate 0:00:00"
+echo # Just some extra spacing
+echo "Files older than $oldDate are"
+find . -type f -not -newermt "$oldDate 0:00:00"
+# -not = Essentially gets older than specified date.
