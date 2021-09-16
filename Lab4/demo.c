dc07897fefdd5c3aa14fcd5794d7e0cc8220ce4c
Sameep1234 <sameep0112vani@gmail.com>
2021-09-16 16:16:10 +0530

    Lab 4 Copy program with no access rights to the new file

--- Lab4/demo.c
+++ Lab4/demo.c
@@ -4,10 +4,20 @@
 #include<unistd.h>
 #include<stdio.h>
 
+// Copy files program
+
 int main(void)
 {
-    int x = open("demo.txt", O_RDONLY);
-    close(x);
+    int fd = open("demo.txt", O_RDONLY);
+    char buf[BUFSIZ] = {0};
+
+    read(fd, buf, BUFSIZ);
+
+    int new_fd = open('copied_demo.txt', O_WRONLY | O_CREAT);
+    write(new_fd, buf, BUFSIZ);
+
+    close(fd);
+    close(new_fd);
     return 0;
 }
 
