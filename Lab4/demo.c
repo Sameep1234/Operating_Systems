10d93f3b171cc033986cad9144fdc7b3dd44f679
Sameep1234 <sameep0112vani@gmail.com>
2021-09-16 16:53:25 +0530

    Added access rights to the new files

--- Lab4/demo.c
+++ Lab4/demo.c
@@ -1,4 +1,5 @@
 #include<fcntl.h>
+#include<stdlib.h>
 #include<sys/stat.h>
 #include<sys/types.h>
 #include<unistd.h>
@@ -6,15 +7,25 @@
 
 // Copy files program
 
+/*Do the following
+ Proper error handling
+ Loop
+ Recursive version
+*/
+
 int main(void)
 {
     int fd = open("demo.txt", O_RDONLY);
     char buf[BUFSIZ] = {0};
 
-    read(fd, buf, BUFSIZ);
+    int num = read(fd, buf, BUFSIZ);
 
-    int new_fd = open('copied_demo.txt', O_WRONLY | O_CREAT);
-    write(new_fd, buf, BUFSIZ);
+    int new_fd = open('copied_demo.txt', O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+    if(num == -1) {
+        perror("read error");
+        exit(1);
+    }
+    write(new_fd, buf, num);
 
     close(fd);
     close(new_fd);
