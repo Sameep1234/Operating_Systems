e68b0c22ba6c915c0ff6c0282b5d85b85094e806
Sameep1234 <sameep0112vani@gmail.com>
2021-09-23 23:17:42 +0530

    Completed non-recursive codes for all 5 questions

---
+++ Lab4/ls.c
@@ -0,0 +1,41 @@
+#include <dirent.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/syscall.h>
+
+struct linux_dirent
+{
+    long d_ino;
+    off_t d_off;
+    unsigned short d_reclen;
+    char d_name[];
+};
+
+int main(int argc, char *argv[])
+{
+    int fd, nread;
+    char buf[BUFSIZ];
+    struct linux_dirent *d;
+    int bpos;
+    char d_type;
+
+    fd = open(argc > 1 ? argv[1] : ".", O_RDONLY | O_DIRECTORY);
+    if (fd == -1)
+        perror("open");
+
+    nread = syscall(SYS_getdents, fd, buf, BUFSIZ);
+    if (nread == -1)
+        perror("getdents");
+
+    while(bpos < nread)
+    {
+        d = (struct linux_dirent *)(buf + bpos);
+        printf("%s\n", d->d_name);
+        bpos += d->d_reclen;
+    }
+
+    exit(EXIT_SUCCESS);
+}
--- Lab4/stat.c
+++ Lab4/stat.c
@@ -1,11 +1,50 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<fcntl.h>
-#include<sys/types.h>
-#include<sys/stat.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
-int main(void) {
-    
+int main(void)
+{
+    struct stat sfile;
+    const char *fileName = "demo.c";
+    int fd = open(fileName, O_RDONLY);
+    if (fd == -1)
+    {
+        perror("File did not open!");
+        exit(EXIT_FAILURE);
+    }
+    if (stat(fileName, &sfile) == -1)
+    {
+        perror("No Stat!");
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        printf("\nFile st_uid %d \n", sfile.st_uid);
+        printf("\nFile st_blksize %ld \n", sfile.st_blksize);
+        printf("\nFile st_gid %d \n", sfile.st_gid);
+        printf("\nFile st_blocks %ld \n", sfile.st_blocks);
+        printf("\nFile st_size %ld \n", sfile.st_size);
+        printf("\nFile st_nlink %u \n", (unsigned int)sfile.st_nlink);
+        printf("\nFile Permissions User\n");
+        printf((sfile.st_mode & S_IRUSR) ? "r" : "-");
+        printf((sfile.st_mode & S_IWUSR) ? "w" : "-");
+        printf((sfile.st_mode & S_IXUSR) ? "x" : "-");
+        printf("\n");
+        printf("\nFile Permissions Group\n");
+        printf((sfile.st_mode & S_IRGRP) ? "r" : "-");
+        printf((sfile.st_mode & S_IWGRP) ? "w" : "-");
+        printf((sfile.st_mode & S_IXGRP) ? "x" : "-");
+        printf("\n");
+        printf("\nFile Permissions Other\n");
+        printf((sfile.st_mode & S_IROTH) ? "r" : "-");
+        printf((sfile.st_mode & S_IWOTH) ? "w" : "-");
+        printf((sfile.st_mode & S_IXOTH) ? "x" : "-");
+        printf("\n");
+    }
+
+    printf("st_mode = %lo", sfile.st_ino);
     return 0;
 }
