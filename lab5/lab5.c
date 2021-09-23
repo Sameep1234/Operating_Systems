58094d18871361d7c5d2cd4c48920b889ba04b8f
Sameep1234 <sameep0112vani@gmail.com>
2021-09-23 15:41:35 +0530

    Docs: Child process and parent process with ids

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -5,6 +5,7 @@
 int main(void)
 {
     int x = fork(); // Creates child process. After this, all the code inside this process will be copied by the child.
+    int y = '123'; // After fork, this will be in both stacks (child and process).
     // Returns the process id of child to parent. So for child, the process id is 0 and for parent it is x
     // NOTE THAT THE CHILD PROCESS ID WILL ALWAYS BE 0.
     /*
@@ -15,7 +16,11 @@ int main(void)
         perror("fork");
         exit(EXIT_FAILURE);
     } else if(x == 0) {
-        printf("%d\n", 2); // Prints only for child (access only to child)
+        y++; // This will change value of y only in child's stack and not in parent's stack.
+        printf("Only in child"); // Prints only for child (access only to child)
+        printf("Child Process id: %d", getpid()); // This will not be 0 but returns its original process id.
+        // NOTE THAT THE X WILL ALWAYS BE 0.
+        printf("Parent Process id: %d", getppid());// This will be the process id of parent.
     } else {
         printf("%d\n", 4); // Prints only for parent (access only to parent process)
     }
