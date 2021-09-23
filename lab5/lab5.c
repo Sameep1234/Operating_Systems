601e83e5356e23cbf362ad3067f665414e7e71dd
Sameep1234 <sameep0112vani@gmail.com>
2021-09-23 15:03:25 +0530

    Docs: Child process and parent process

---
+++ lab5/lab5.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+int main(void)
+{
+    int x = fork(); // Creates child process. After this, all the code inside this process will be copied by the child.
+    // Returns the process id of child to parent. So for child, the process id is 0 and for parent it is x
+    // NOTE THAT THE CHILD PROCESS ID WILL ALWAYS BE 0.
+    /*
+        Copied part: Stack, PCB, Code. Hence value of x is stored in both child and process.
+    */
+    printf("%d\n", x);
+    if(x == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    } else if(x == 0) {
+        printf("%d\n", 2); // Prints only for child (access only to child)
+    } else {
+        printf("%d\n", 4); // Prints only for parent (access only to parent process)
+    }
+
+    printf("Here\n"); // Prints 2 times (for child and for parent)
+    return 0;
+}
