9626a20ac73f2a09c577154d2d4f3561e1c0b5c1
Sameep1234 <sameep0112vani@gmail.com>
2021-09-25 18:19:59 +0530

    Stat done properly

--- Lab4/stat.c
+++ Lab4/stat.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <time.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
@@ -8,7 +9,7 @@
 int main(void)
 {
     struct stat sfile;
-    const char *fileName = "/home/sameep/Extra Projects/Operating_Systems/Lab4";
+    const char *fileName = "/home/sameep/Extra Projects/Operating_Systems/Lab4/demo.c";
     int fd = open(fileName, O_RDONLY);
     if (fd == -1)
     {
@@ -22,37 +23,10 @@ int main(void)
     }
     else
     {
-        if(sfile.st_mode & S_IFDIR)
-        {
-            printf("It is a directory");
-        }
-        else 
-        {
-            printf("It is a file");
-        }
-        // printf("\nFile st_uid %d \n", sfile.st_uid);
-        // printf("\nFile st_blksize %ld \n", sfile.st_blksize);
-        // printf("\nFile st_gid %d \n", sfile.st_gid);
-        // printf("\nFile st_blocks %ld \n", sfile.st_blocks);
-        // printf("\nFile st_size %ld \n", sfile.st_size);
-        // printf("\nFile st_nlink %u \n", (unsigned int)sfile.st_nlink);
-        // printf("\nFile Permissions User\n");
-        // printf("%c", sfile.st_mode);
-        // printf((sfile.st_mode & S_IWUSR) ? "w" : "-");
-        // printf((sfile.st_mode & S_IXUSR) ? "x" : "-");
-        // printf("\n");
-        // printf("\nFile Permissions Group\n");
-        // printf((sfile.st_mode & S_IRGRP) ? "r" : "-");
-        // printf((sfile.st_mode & S_IWGRP) ? "w" : "-");
-        // printf((sfile.st_mode & S_IXGRP) ? "x" : "-");
-        // printf("\n");
-        // printf("\nFile Permissions Other\n");
-        // printf((sfile.st_mode & S_IROTH) ? "r" : "-");
-        // printf((sfile.st_mode & S_IWOTH) ? "w" : "-");
-        // printf((sfile.st_mode & S_IXOTH) ? "x" : "-");
-        // printf("\n");
+        char buf[BUFSIZ] = {0};
+        snprintf(buf, BUFSIZ, "size: %ld\nblocks: %ld\nIO Block: %ld\nDevice: %lu\nInode: %lu\nLinks: %lu\nUid: %d\nGid: %d\nAccess: %s\nModify: %s\nStatus Change: %s\n", sfile.st_size, sfile.st_blocks, sfile.st_blksize, sfile.st_dev, sfile.st_ino, sfile.st_nlink, sfile.st_uid, sfile.st_gid, ctime(&sfile.st_atime), ctime(&sfile.st_mtime), ctime(&sfile.st_ctime));
+        write(1, buf, sizeof(buf));
     }
-
-    // printf("st_mode = %lo", sfile.st_ino);
+    close(fd);
     return 0;
 }
