3af3853bb902570732418f4b0b94afa6219dc9c0
Sameep1234 <sameep0112vani@gmail.com>
2021-10-21 15:01:38 +0530

    Lab7 added

---
+++ Lab7/lab7_1.c
@@ -0,0 +1,10 @@
+#include <dirent.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
