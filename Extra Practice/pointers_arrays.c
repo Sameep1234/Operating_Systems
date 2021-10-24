8c7b15fc76b53f2d3bcb1a9866b8ae8efb260992
Sameep1234 <sameep0112vani@gmail.com>
2021-10-24 10:41:32 +0530

    Pointers array

--- Extra Practice/pointers_arrays.c
+++ Extra Practice/pointers_arrays.c
@@ -14,6 +14,22 @@
 
 int main()
 {
-    
+    int x[5] = {1, 2, 3, 4, 5}; // Continuous block of memory is created
+    int *p = &x[0]; // If we know this, we know the address of all the elements as it is a continuous block.
+    printf("%d\n", p);
+
+    /* STRINGS */
+    char *str = "good"; // This points to the first char i.e g and not to the whole word.
+    printf("%s\n", str);
+
+    /* Array of pointers (chain of pointers concept) */
+    char *name[3] = {"New Zealand", "Australia", "India"};
+    for(int i = 0;i<3;i++)
+    {
+        printf("%s ", name[i]);
+        if(i == 2)
+            printf("\n");
+    }
+
     return 0;
 }
